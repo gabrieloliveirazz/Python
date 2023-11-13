9701df9f507b09dd8c06eade37706f33d064fe21
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-11-13 10:05:04 -0300

    Create aula1.py

---
+++ aula1.py
@@ -0,0 +1,12 @@
+# - Declara um Comentário
+print(123) 
+
+"""
+DocString
+As 3 aspas declara uma DocString onde se pode 
+pular linhas  
+"""
+
+'''
+Outra maneira de declarar DocString é com aspas simples
+'''
