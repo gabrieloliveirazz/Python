ad5428b66562ae80aa9031b559ab536df5b6dce4
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-11-13 16:59:57 -0300

    Create aula13.py

---
+++ aula13.py
@@ -0,0 +1,13 @@
+nome = 'Gabriel'
+altura = 1.68
+peso = 58
+imc = peso / (altura * altura)
+
+
+linh_1 = f'{nome} tem {altura:.2f} de altura'
+linha_2 = f'pesa {peso} quilos e seu imc é de:'
+linha_3 = f'{imc:.2f}'
+
+print(linh_1)
+print(linha_2)
+print(linha_3)
