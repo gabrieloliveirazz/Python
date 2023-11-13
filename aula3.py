2cff08df5a25e26c0be6b220bdd73c2c444f4f2e
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-11-13 10:24:43 -0300

    Create aula3.py

---
+++ aula3.py
@@ -0,0 +1,17 @@
+'''
+Python -> Linguagem de programação 
+Tipo de Tipagem = Dinâmica / Forte
+str -> string -> texto
+String são textos que estão dentro de aspas 
+'''
+
+# Aspas simples
+print('Gabriel Oliveira')
+# Aspas duplas
+print("Rayanne Nayara")
+
+# Escape
+print('Gabriel \'Oliveira\'')
+
+#r
+print(r"Rayanne \'Nayara\'")
