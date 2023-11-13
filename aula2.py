a961c9cecfa009c69c55f6c357c3848c38b33895
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-11-13 10:05:01 -0300

    Create aula2.py

---
+++ aula2.py
@@ -0,0 +1,5 @@
+print(12, 34, 1011, sep="-", end="##")
+print(56, 78, sep='-', end="\n")
+
+# sep permite que você escolha a forma de separar as linhas 
+# end permite que você escolha a forma de terminar a linha
