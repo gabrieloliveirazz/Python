896646dad57448f61d5a54f05a782c976cd7def5
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-11-13 11:47:55 -0300

    Create aula8.py

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Gabriel'
+sobrenome = 'Oliveira'
+idade = 16
+ano_nascimento = 2007
+maior_idade = idade >= 18
+altura = 1.68
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('É maior de idade: ', maior_idade)
+print('Altura em metros: ', altura)
