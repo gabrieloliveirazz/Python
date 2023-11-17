c5d72673a107fc2d073a2af95903a48d1ddb4964
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-11-17 16:28:07 -0300

    Create aula16.py

---
+++ aula16.py
@@ -0,0 +1,11 @@
+# if  /    elif    / else 
+# se / se não se  / se não 
+
+entrada = input('Você que "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+else:
+    print('Você não digitou nenhuma das opçõe.')
