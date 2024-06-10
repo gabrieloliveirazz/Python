514b4163d835bef5cc8b2e0f36eaf3c44b8ffb53
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2024-06-10 17:22:39 -0300

    a

--- aula33.py
+++ aula33.py
@@ -3,4 +3,7 @@ https://docs.python.org/pt-br/3/library/stdtypes.html
 Imutáveis que vimos: str, int, float, bool
 """
 
-string = 'luiz otaviio'
+string = 'luiz otavio'
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+print(string)
+print(outra_variavel)
---
+++ aula34.py
@@ -0,0 +1,12 @@
+"""
+Repetições
+While (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> quando um código não tem fim
+"""
+condicao = True
+
+while condicao:
+    nome = input('qual o seu nome? ')
+    print(f'seu nome é {nome}')
+    
